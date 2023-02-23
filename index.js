6944dcfead3bfb2344f73ca9ffb9a995001190d5
GGrop <giuliano.gropponi@gmail.com>
2023-02-23 20:17:38 -0300

    add funciones iniciales turnoMaquina, obtenerCuadradoAleatorio, resaltar
    avance hasta tener terminada la funcion Resaltar q me logra resaltar los cuadrados de la secuencia de la maquina

--- index.js
+++ index.js
@@ -15,3 +15,59 @@ ejemplo de flujo:
 4-gane?     repito paso 2 y 3 con + dificultad  :  perdiste 
 5-ofrecer empezar de nuevo
 */
+document.querySelector('#empezar').onclick = function(e){
+    empezarJuego()
+}
+let secuenciaMaquina = []
+let secuenciaJugador = []
+let nivel = 0
+
+const empezarJuego=()=>{
+    turnoMaquina()
+}
+
+const turnoMaquina=()=>{
+    const cuadradoAleatorio = obtenerCuadradoAleatorio()
+    secuenciaMaquina.push(cuadradoAleatorio)
+    ejecucionTurnoMaquina(secuenciaMaquina)
+}
+
+const obtenerCuadradoAleatorio=()=>{
+    const $cuadrados = document.querySelectorAll('.cuadrado')
+    let indice = Math.floor(Math.random()* $cuadrados.length)
+    return $cuadrados[indice]
+}
+
+const ejecucionTurnoMaquina=(secuencia)=>{
+    secuencia.forEach(function(secuencia,i){
+        let DELAY = 1000*i
+        setTimeout(function(){
+            resaltar(`#${secuencia.id}`)
+        },DELAY)
+    })
+}
+
+const resaltar=(cuadradoId)=>{
+    document.querySelector(cuadradoId).style.opacity = 1
+    setTimeout(function(){
+        document.querySelector(cuadradoId).style.opacity = 0.5
+    },500)
+}
+
+const estadoDeJuego=(texto)=>{
+    const $alert = document.querySelector('#estadoDeJuego')
+    $alert.classList.add('alert-primary');
+        if(/Perdiste/.test(texto)){
+            $alert.classList.remove('alert-primary');
+            $alert.classList.add('alert-danger');
+            $alert.textContent = texto
+        }
+        if(/Ganaste/.test(texto)){
+            $alert.classList.remove('alert-primary');
+            $alert.classList.add('alert-success');
+            $alert.textContent = texto
+        }
+
+    }
+// estadoDeJuego('Perdiste :( vuelvelo a intentar!')
+// estadoDeJuego('Ganaste! :D podes volver a jugar clickeando en empezar!')
