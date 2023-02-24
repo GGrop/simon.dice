9d34312771daeb7c257f103d6df678bed48a6994
GGrop <giuliano.gropponi@gmail.com>
2023-02-24 14:09:01 -0300

    borra croquis comentando innecesario

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-/*
-croquis:
-
-    interfaz:
-    1-presiona empezar para jugar!/perdiste/ganaste
-    2-Indicador de nivel
-    3-boton empezar
-    4-4 cajas con colores distintos
-//////////////////
-ejemplo de flujo:
-    1-clickeo boton empezar
-    2-turno de la maquina
-    3-turno del usuario
-    4-gane?     repito paso 2 y 3 con + dificultad  :  perdiste 
-    5-ofrecer empezar de nuevo
-*/
 let secuenciaMaquina = []
 let secuenciaJugador = []
 let nivel = 0
